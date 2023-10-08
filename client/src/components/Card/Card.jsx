70fa24fa3dd488021f0114694298607c90d2c56e
CLoboguerrero <c.loboguerrero@outlook.com>
2023-10-07 22:47:22 -0600

    Controllers/Handlers Terminados y Renderizado Correcto de Cards

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ function Card ({ id, name, image, genres }) {
         <div className='card'>
             <img className='img-card' src={image} alt='game-image' />
             <h2 className='game-name'>{name}</h2>
-            <h2 className='game-genres'><span style={{ fontWeight: 'bold' }}>Genres: </span>{genres.join(', ')}</h2>
+            <h2 className='game-genres'><span style={{ fontWeight: 'bold' }}>Genres: </span>{genres.join(' ')}</h2>
 
             <div className='bottom-buttons'>
                 <Link to={`/detail/${id}`} >
