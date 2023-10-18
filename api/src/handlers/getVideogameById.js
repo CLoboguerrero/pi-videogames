8166ca8f80ebbf58d51d0df7bb08b13929fbb679
CLoboguerrero <c.loboguerrero@outlook.com>
2023-10-17 18:11:21 -0600

    Ligeros Cambios a Controller

--- api/src/handlers/getVideogameById.js
+++ api/src/handlers/getVideogameById.js
@@ -4,16 +4,21 @@ const { Videogame, Genre } = require('../db');
 const { API_KEY } = process.env;
 const { Op } = require('sequelize');
 
-const isUUID = (id) => {
-    const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    return uuidRegex.test(id);
-};
+// const isUUID = (id) => {
+//     const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+//     return uuidRegex.test(id);
+// };
 
 const getVideogameById = async (req, res) => {
     const { id } = req.params;
 
+    let idType = 'number';
+    if(isNaN(id)) {
+        idType = 'uuid';
+    }
+
     try {
-        if (isUUID(id)) {
+        if (idType === 'uuid') {
      
             const gameInDb = await Videogame.findOne({
                 where: {
@@ -67,58 +72,3 @@ const getVideogameById = async (req, res) => {
 }
 
 module.exports = { getVideogameById }
-
-
-
-
-
-
-// const axios = require('axios');
-// const { sequelize } = require('../db');
-// const { Videogame, Genre } = require('../db');
-// const { API_KEY } = process.env;
-
-
-// const getVideogameById = async (req, res) => {
-//     const { id } = req.params;
-
-//     try {
-//         const gameInDb = await Videogame.findOne({
-//             where: {
-//                 id: id,
-//             },
-//             include: {
-//                 model: Genre,
-//                 attributes: ['id', 'name'],
-//                 through: {
-//                     attributes: [],
-//                 }
-//             }
-//         });
-
-//         if (gameInDb){
-//             return res.status(200).json(gameInDb);
-//         } else {
-//             const response = await axios.get(`https://api.rawg.io/api/games/${id}?key=${API_KEY}`);
-//             const game = response.data;
-
-//             const gameInApi = {
-//                 id: game.id,
-//                 name: game.name,
-//                 background_image: game.background_image,
-//                 description: game.description,
-//                 released: game.released,
-//                 rating: game.rating,
-//                 genres: game.genres.map(genre => genre.name),
-//                 platforms: game.platforms.map(platform => platform.platform.name),
-            
-//             };
-//             return res.status(200).json(gameInApi);
-//         };
-//     } catch (error) {
-//         console.error('Error fetching Data:', error);
-//         return res.status(500).json({ error: 'Internal Server error' });
-//     }
-// }
-
-// module.exports = { getVideogameById }
